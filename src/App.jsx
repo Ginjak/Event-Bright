1c72e9aef6a89229cf7cc375b06c28724d54e93c
Adrian Tut <adriantutuianu01@gmail.com>
2024-02-28 20:00:36 +0000

    Fixed Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
