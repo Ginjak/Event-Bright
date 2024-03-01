c01734f1eb137ebfbd5c5b9489fc73762168d82e
daria <darias2002@gmail.com>
2024-03-01 22:12:59 +0000

    additional fixes and code cleanup

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -9,53 +9,49 @@ const Footer = () => {
 
   return (
     <>
-    <div className="footer">
-      <nav className="navbar bg-body-tertiary">
-        <div className='m-5'>
-          <div className="container-fluid">
+      <nav className="navbar bg-body-tertiary bg-image bg-opacity">
+        <div className='p-3'>
+          <div className="container-fluid p-2">
             <a className="navbar-brand logo" href="#">
               <img src="/eventBright-logo.png" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
-              <span>EventBright</span>
+              <span className=''>EventBright</span>
             </a>
           </div>
         </div>
         {/* social medias >> */}
         <div className="socials-container">
-          <h3 className="fs-6 margins">Future links </h3>
+          <span className="fs-6 p-3 ms-5 fw-bold">Future links</span>
           <div className="socials-list">
-            <a href="#" className="">
-              <InstagramIcon fontSize='large' className='link'/>
+            <a href="#">
+              <InstagramIcon fontSize='large' className='link icons spacing'/>
             </a>
-            <a href="#" className="">
-              <FacebookIcon fontSize='large' className='link'/>
+            <a href="#">
+              <FacebookIcon fontSize='large' className='link icons spacing'/>
             </a>
-            <a href="#" className="">
-              <YouTubeIcon fontSize='large' className='link'/>
+            <a href="#">
+              <YouTubeIcon fontSize='large' className='link icons spacing'/>
             </a>
-            <a href="#" className="">
-              <GitHubIcon fontSize='large' className='link'/>
+            <a href="#">
+              <GitHubIcon fontSize='large' className='link icons spacing'/>
             </a>
           </div>
         </div>
         <div className="pages-container">
-          <div className="page-links">
-            <span className="margins">Explore: </span>
-            <a href="#" className="link">
+          <span className="fs-6 p-3 fw-bold">Explore </span>
+            <a href="#" className="link spacing">
               About
             </a>
-            <a href="#" className="link">
+            <a href="#" className="link spacing">
               Contact
             </a>
-            <a href="#" className="link">
+            <a href="#" className="link spacing">
               Weather
             </a>
-          </div>
         </div>
       </nav>
       <div className='date-container'>
           <p>&copy; {new Date().getFullYear()} EventBright</p>
       </div>
-    </div>
     </> 
 )
 };
