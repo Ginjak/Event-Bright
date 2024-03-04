0a8eda3caaafa3e76052c3697115909ebe306a72
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-03-04 19:11:33 +0000

    Added comments to Upcomingevents component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
