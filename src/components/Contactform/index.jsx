78fe787833123cabfd9b01400ba80bcd56c8b494
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-03-05 21:23:20 +0000

    Styling changes to contact page

--- src/components/Contactform/index.jsx
+++ src/components/Contactform/index.jsx
@@ -1,101 +1,133 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import "./contactForm.css";
 
 const Contactform = () => {
   return (
     <>
-      <div className="p-5 mb-4 rounded-3">
+      <section className="contact-details text-white py-5 px-3">
+        {/* <div className="p-5 mb-4 rounded-3"> */}
         <div className="container-xxl">
           <div className="row">
-            <div className="col-6">
-              <h3>
-                <strong>Address</strong>
-              </h3>
-              <p>
-                123 Fake St
-                London
-                EVE NT5
-              </p>
-              <h3>
-                <strong>Email</strong>
-              </h3>
-              <p>EventBright@gmail.com</p>
-              <h3>
-                <strong>Follow us on socials</strong>
-              </h3>
+            <div className="col-md-6">
+              <h3>Address</h3>
+              <p className="m-0">123 Fake St</p>
+              <p className="m-0">London</p>
+              <p className="mb-2">EVE NT5</p>
+              <h3>Email</h3>
+              <NavLink
+                to="mailto:eventbright@gmail.com"
+                className="text-white mail-link"
+              >
+                info@eventbright.com
+              </NavLink>
+
+              <h3 className="mt-2">Follow us on socials</h3>
+              <div className="social-links">
+                <NavLink to="https://www.instagram.com/" target="_blank">
+                  <InstagramIcon
+                    fontSize="large"
+                    className=" icons spacing text-white ms-0 mail-link"
+                  />
+                </NavLink>
+                <NavLink to="https://www.facebook.com/" target="_blank">
+                  <FacebookIcon
+                    fontSize="large"
+                    className=" icons spacing text-white"
+                  />
+                </NavLink>
+                <NavLink to="https://www.youtube.com/" target="_blank">
+                  <YouTubeIcon
+                    fontSize="large"
+                    className=" icons spacing text-white"
+                  />
+                </NavLink>
+                <NavLink
+                  to="https://github.com/Ginjak/Event-Bright"
+                  target="_blank"
+                >
+                  <GitHubIcon
+                    fontSize="large"
+                    className=" icons spacing text-white"
+                  />
+                </NavLink>
+              </div>
             </div>
 
-            <div className="col-6">
-              <div className="container-fluid">
-                <div className="contact-form">
-                  <h2>Contact Us</h2>
-                  <form>
-                    <div className="form-group">
-                      <label htmlFor="name">
-                        <strong>Your Name: </strong>
-                      </label>
-                      <br />
-                      <input
-                        className="form-control"
-                        type="text"
-                        id="name"
-                        name="name"
-                        required
-                      />
-                    </div>
+            <div className="mt-5 mt-md-0 col-md-6">
+              <div className="contact-form">
+                {/* <h2>Contact Us</h2> */}
+                <form>
+                  <div className="form-group">
+                    <label className="fw-bold" htmlFor="name">
+                      Your Name:
+                    </label>
                     <br />
-                    <div className="form-group">
-                      <label htmlFor="email">
-                        <strong>Your Email:</strong>{" "}
-                      </label>
-                      <br />
-                      <input
-                        className="form-control"
-                        type="email"
-                        id="email"
-                        name="email"
-                        required
-                      />
-                    </div>
+                    <input
+                      className="form-control"
+                      type="text"
+                      id="name"
+                      name="name"
+                      required
+                    />
+                  </div>
+                  <br />
+                  <div className="form-group">
+                    <label className="fw-bold" htmlFor="email">
+                      Your Email:
+                    </label>
                     <br />
-                    <div className="form-group">
-                      <label htmlFor="subject">
-                        <strong>Subject:</strong>{" "}
-                      </label>
-                      <br />
-                      <input
-                        className="form-control"
-                        type="subject"
-                        id="subject"
-                        name="subject"
-                        required
-                      />
-                    </div>
+                    <input
+                      className="form-control"
+                      type="email"
+                      id="email"
+                      name="email"
+                      required
+                    />
+                  </div>
+                  <br />
+                  <div className="form-group">
+                    <label className="fw-bold" htmlFor="subject">
+                      Subject:
+                    </label>
                     <br />
-                    <div className="form-group">
-                      <label htmlFor="message">
-                        <strong>Message:</strong>{" "}
-                      </label>
-                      <br />
-                      <textarea
-                        className="form-control"
-                        id="message"
-                        name="message"
-                        rows="4"
-                        required
-                      />
-                    </div>
+                    <input
+                      className="form-control"
+                      type="subject"
+                      id="subject"
+                      name="subject"
+                      required
+                    />
+                  </div>
+                  <br />
+                  <div className="form-group">
+                    <label className="fw-bold" htmlFor="message">
+                      Message:
+                    </label>
                     <br />
-                    <button className="formButton" type="submit">
-                      <strong>Submit</strong>
-                    </button>
-                  </form>
-                </div>
+                    <textarea
+                      className="form-control"
+                      id="message"
+                      name="message"
+                      rows="4"
+                      required
+                    />
+                  </div>
+                  <br />
+                  <button className="btn-contact" type="submit">
+                    <strong>Submit</strong>
+                  </button>
+                </form>
               </div>
             </div>
           </div>
         </div>
-      </div>
+        {/* </div> */}
+      </section>
     </>
   );
 };
